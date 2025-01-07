ecb93872fdb17cb14d58728e98d93ba0bfeffe9d
Elton da Costa Oliveira <eltoncosta00614@gmail.com>
2025-01-07 12:29:42 -0300

    feat: add Earth-Moon-Earth travel choice logic in screen

--- main.py
+++ main.py
@@ -99,6 +99,40 @@ class JupiterViagens(Screen):
     pass
 
 
+class LuaViagens(Screen):
+    def carregar_lua_viagens(self):
+        self.contador = 1
+        for linha in self.datas:  # para cada linha no arquivo csv, irei criar um botão contendo as infos da viagem
+            data = ler_datas(linha)
+            btn = Button(text=f"[{self.contador}] Início da viagem: {data[0]}, Chegada em Marte: {data[1]}"
+                              f", Fim da viagem: {data[2]}, Duração total:{data[3]} dias",
+                         on_press=lambda instance, c=self.contador: self.salvar_viagem(instance, c)
+                         )
+            self.ids.viagens_grid.add_widget(btn)
+            self.contador += 1
+
+    def salvar_viagem(self, instance, viagemescolhida):
+        dados_viagem_escolhida = ler_datas(self.datas[viagemescolhida - 1])
+        # Transformando viagem .csv em objeto Viagem
+        viagem = Viagem(
+            dados_viagem_escolhida[0],
+            converter_str_date(dados_viagem_escolhida[1]),
+            converter_str_date(dados_viagem_escolhida[2]),
+            converter_str_date(dados_viagem_escolhida[3]),
+            int(dados_viagem_escolhida[4]),
+            float(dados_viagem_escolhida[5])
+        )
+
+        # salvando viagem escolhida
+        open("viagens_reservadas.csv", "a").writelines(viagem.converter_csv())
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.datas = open("terra-lua-terra.csv").readlines()
+        self.carregar_lua_viagens()
+    pass
+
+
 class MainApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -112,6 +146,7 @@ class MainApp(App):
         self.sm.add_widget(PlanejarViagem(name="PlanejarViagem"))
         self.sm.add_widget(MarteViagens(name="MarteViagens"))
         self.sm.add_widget(JupiterViagens(name="JupiterViagens"))
+        self.sm.add_widget(LuaViagens(name="LuaViagens"))
         return self.sm
 
     def tela_menu_planejar(self, instance):
@@ -125,6 +160,9 @@ class MainApp(App):
 
     def tela_jupiter_viagens(self, instance):
         self.sm.current = "JupiterViagens"
+
+    def tela_lua_viagens(self, instance):
+        self.sm.current = "LuaViagens"
 
 
 def formatar_texto(txt: str):
@@ -142,7 +180,7 @@ formatar_texto("Voyager Trip - Planejador de Viagens")
 print("O que você deseja fazer?\n[1] Ver viagens planejadas\n[2] Planejar viagem")
 opcao = int(input("Digite aqui:"))
 
-match opcao:  # calculo da duração da viagem de ida
+match opcao:
     case 1:  # salvar viagem
         viagens_reservadas = []  # viagens salvas serão armazenadas nessa lista, para depois eu pecorre-la
         file = open("viagens_reservadas.csv").readlines()
@@ -165,66 +203,6 @@ match opcao:  # calculo da duração da viagem de ida
             for viagem in viagens_reservadas:
                 print(viagem.__str__())
                 print("=" * 20)
-
-    case 2:
-        print("[1] Lua\n[2] Marte\n[3] Júpiter")
-        destino = int(input("Selecione uma opção"))
-
-        if destino == 1:
-            datas = open("terra-lua-terra.csv").readlines()
-            c = 1
-            for line in datas:
-                data = ler_datas(line)
-                print(f"[{c}] Início da viagem: {data[0]}, Chegada na Lua: {data[1]}"
-                      f", Fim da viagem: {data[2]}, Duração total:{data[3]} dias")
-                c += 1
-            viagem_escolhida = int(input("Selecione uma das opções de viagem listadas: "))
-            print("=" * 20)
-            print("Viagem escolhida")
-            dados_viagem_escolhida = ler_datas(datas[viagem_escolhida - 1])
-            # Transformando viagem .csv em objeto Viagem
-            viagem = Viagem(
-                dados_viagem_escolhida[0],
-                converter_str_date(dados_viagem_escolhida[1]),
-                converter_str_date(dados_viagem_escolhida[2]),
-                converter_str_date(dados_viagem_escolhida[3]),
-                int(dados_viagem_escolhida[4]),
-                float(dados_viagem_escolhida[5])
-            )
-            # Armazenamento
-            armazenamento = open("viagens_reservadas.csv", "a").writelines(
-                [viagem.destino + "," + str(viagem.data_inicio) + "," + str(viagem.data_chegada_destino) + "," +
-                 str(viagem.data_chegada_retorno) + "," + str(viagem.duracao) + "," + str(viagem.delta_v) + ",\n"]
-            )
-            formatar_texto("Boa viagem :)")
-
-        elif destino == 3:
-            datas = open("terra-jupiter-terra.csv").readlines()
-            c = 1
-            for line in datas:
-                data = ler_datas(line)
-                print(f"[{c}] Início da viagem: {data[0]}, Chegada em Jupiter: {data[1]}"
-                      f", Fim da viagem: {data[2]}, Duração total:{data[3]} dias")
-                c += 1
-            viagem_escolhida = int(input("Selecione uma das opções de viagem listadas: "))
-            print("=" * 20)
-            dados_viagem_escolhida = ler_datas(datas[viagem_escolhida - 1])
-            # Transformando viagem .csv em objeto Viagem
-            viagem = Viagem(
-                dados_viagem_escolhida[0],
-                converter_str_date(dados_viagem_escolhida[1]),
-                converter_str_date(dados_viagem_escolhida[2]),
-                converter_str_date(dados_viagem_escolhida[3]),
-                int(dados_viagem_escolhida[4]),
-                float(dados_viagem_escolhida[5])
-            )
-            # Armazenamento
-            armazenamento = open("viagens_reservadas.csv", "a").writelines(
-                [viagem.destino + "," + str(viagem.data_inicio) + "," + str(viagem.data_chegada_destino) + "," +
-                 str(viagem.data_chegada_retorno) + "," + str(viagem.duracao) + "," + str(viagem.delta_v) + ",\n"]
-            )
-            print("Viagem escolhida")
-            formatar_texto("Boa viagem :)")
 
 if __name__ == '__main__':
     app = MainApp()
